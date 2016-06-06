1f27c60e2e2016c98da4e2ff8e35c425fca0c83d
wenzhang <zhangwenting111@gmail.com>
2016-06-05 21:46:42 -0400

    static

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,12 +39,12 @@ module.exports = function(grunt) {
                 //that's the path where the snapshots should be placed
                 //it's empty by default which means they will go into the directory
                 //where your Gruntfile.js is placed
-                snapshotPath: 'snapshots/',
+                snapshotPath: '',
                 //This should be either the base path to your index.html file
                 //or your base URL. Currently the task does not use it's own
                 //webserver. So if your site needs a webserver to be fully
                 //functional configure it here.
-                sitePath: 'http://localhost:8080/',
+                // sitePath: 'http://localhost:8080/',
                 //you can choose a prefix for your snapshots
                 //by default it's 'snapshot_'
                 fileNamePrefix: 'sp_',
@@ -64,36 +64,26 @@ module.exports = function(grunt) {
                 },
                 //if you would rather not keep the script tags in the html snapshots
                 //set `removeScripts` to true. It's false by default
-                removeScripts: true,
+                // removeScripts: true,
                 //set `removeLinkTags` to true. It's false by default
-                removeLinkTags: true,
+                // removeLinkTags: true,
                 //set `removeMetaTags` to true. It's false by default
-                removeMetaTags: true,
+                // removeMetaTags: true,
                 //Replace arbitrary parts of the html
-                replaceStrings:[
-                    {'this': 'will get replaced by this'},
-                    {'/old/path/': '/new/path'}
-                ],
+                // replaceStrings:[
+                //     {'this': 'will get replaced by this'},
+                //     {'/old/path/': '/new/path'}
+                // ],
                 // allow to add a custom attribute to the body
                 bodyAttr: 'data-prerendered',
                 //here goes the list of all urls that should be fetched
                 urls: [
-                  '',
-                  'day/1',
-                  'day/2',
-                  'day/3',
-                  'day/4',
-                  'day/5',
-                  'day/6',
-                  'day/7',
-                  'day/8',
-                  'day/9',
-                  'day/10'
+                  '/index.html'
                 ],
                 // a list of cookies to be put into the phantomjs cookies jar for the visited page
-                cookies: [
-                  {"path": "/", "domain": "localhost", "name": "lang", "value": "en-gb"}
-                ],
+                // cookies: [
+                //   {"path": "/", "domain": "localhost", "name": "lang", "value": "en-gb"}
+                // ],
                 // options for phantomJs' page object
                 // see http://phantomjs.org/api/webpage/ for available options
                 pageOptions: {
@@ -108,7 +98,7 @@ module.exports = function(grunt) {
         watch: {
             css: {
                 files: 'scss/*.scss',
-                tasks: ['sass', 'autoprefixer', 'htmlSnapshot']
+                tasks: ['sass', 'autoprefixer']
             },
             options: {
               livereload: true
@@ -128,6 +118,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-html-snapshot');
     grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.registerTask('default', ['connect', 'watch']);
+    grunt.registerTask('default', ['connect', 'htmlSnapshot', 'watch']);
 }
 
